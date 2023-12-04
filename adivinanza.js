79ba04e055b50de1ae1f92add4bf7997c903b22d
Agustina <dis.agustinalopez@gmail.com>
2023-12-04 12:58:35 -0300

    Adivinar Numero Secreto

---
+++ adivinanza.js
@@ -0,0 +1,18 @@
+const generarNumeroAleatorio = () =>{
+    return Math.floor(Math.random()*100) + 1;
+};
+
+const verificarAdivinanza = (numeroSecreto, numeroAdivinado) =>{
+    if(numeroAdivinado == numeroSecreto){
+        console.log('¡Felicitaciones! ¡Adivinaste el número secreto!');
+    } else if(numeroAdivinado > numeroSecreto){
+        console.log('El número secreto es menor. ¡Sigue Intentando!');
+    } else {
+        console.log('El número secreto es mayor. ¡Sigue Intentando!');
+    }
+};
+
+module.exports = {
+    generarNumeroAleatorio,
+    verificarAdivinanza
+};
